571a2e063bcd04a2baeb973857bc4939ecd42e72
b00leant <stefanoelatini@hotmail.it>
2020-03-29 17:07:26 +0200

    added submodule for iOS app

---
+++ iOS/Colligo/Colligo/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Colligo
+//
+//  Created by b00leant on 3/28/20.
+//  Copyright © 2020 b00leant. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate, UIScrollViewDelegate, WKUIDelegate,UIGestureRecognizerDelegate {
+    
+    var webView = WKWebView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view = webView
+        
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.allowsInlineMediaPlayback = true
+        
+        webConfiguration.preferences.javaScriptCanOpenWindowsAutomatically = true
+        webConfiguration.preferences.javaScriptEnabled = true
+        //webView = WKWebView(frame: self.view.frame, configuration: webConfiguration)
+        
+        webView.navigationDelegate = self
+        
+        webView.scrollView.delegate = self
+        webView.navigationDelegate = self
+        webView.scrollView.bounces = false
+        //webView.uiDelegate = self
+        
+        if let url = URL(string: "https://vetrina.cloud") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+    // Disable zoom events and gestures
+    func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
+        scrollView.pinchGestureRecognizer?.isEnabled = false
+    }
+    
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return nil
+    }
+
+
+}
+
